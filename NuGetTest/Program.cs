92b0c68ef5ba85b53d324f1a1eae2ff359fd0de6
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2025-06-13 12:09:31 +0200

    GeminiPostSQL NuGet 1.0.2 Update
    Updated the code of the Test Demo to match the changes in GeminiPostSQL NuGet 1.0.2

--- NuGetTest/Program.cs
+++ NuGetTest/Program.cs
@@ -2,27 +2,29 @@
 using Newtonsoft.Json;
 
 // Initialize client with the apiKey and separate database data Builder
-GeminiPostSQLClient client = new GeminiPostSQLClient("API_KEY", "DB_IP", "DB_USER", "DB_PASS", "DB_NAME");
+GeminiPostSQLClient client = new GeminiPostSQLClient("AIzaSyAbeCmpBK9C5JvxqNcOb2LRaWkyltCQ7fI", "192.168.1.14", "postgres", "WJXSBJg.", "betaconsultores");
 
 // Optional parameters
-// client.mapOnGenerateSQL = true; // Map Database automatically in GenerateSQL() method
-// client.runOnGenerateSQL = true; // Run query automatically in GenerateSQL() method
+client.mapOnGenerateSQL = true; // Map Database automatically in GenerateSQL() method
+client.runOnGenerateSQL = true; // Run query automatically in GenerateSQL() method
 
 Console.WriteLine("Mapping database...");
-if (client.MapDatabase().Result) 
+
+if (!client.mapOnGenerateSQL)
+    client.MapDatabase();
+
+Console.WriteLine("Mapping complete.");
+
+Console.WriteLine("Gemini is generating the SQL sentence...");
+string query = client.GenerateSQL("Last albaran of 2025").Result;
+
+if (!client.runOnGenerateSQL)
 {
-    Console.WriteLine("Mapping complete.");
-
-    Console.WriteLine("Gemini is generating the SQL sentence...");
-    string query = client.GenerateSQL("Last albaran of 2025").Result;
     Console.WriteLine("Generated query: " + query);
-
     Console.WriteLine("Running query...");
-    string result = JsonConvert.SerializeObject(client.RunQuery(query).Result);
+    query = JsonConvert.SerializeObject(client.RunQuery(query).Result);
 
-    Console.WriteLine("Result table:\n" + result);
+    Console.WriteLine("Result table:\n" + query);
 }
 else
-{
-    Console.WriteLine("The mapping process failed.");
-}
+    Console.WriteLine("Result table:\n" + query);
